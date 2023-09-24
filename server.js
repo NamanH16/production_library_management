f82ceae99cd074c5ac8a36b1f2aded2c54ea8825
Naman Hiran <106793339+NamanH16@users.noreply.github.com>
2023-09-24 18:18:39 +0530

    bug solved

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
+import cors from 'cors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -15,29 +17,23 @@ dotenv.config()
 
 connectDB()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express()
 
+app.use(cors());
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   )
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....')
-//   })
-// }
 app.use('*',(req,res) => {
   res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
 })
@@ -45,7 +41,7 @@ app.use('*',(req,res) => {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
